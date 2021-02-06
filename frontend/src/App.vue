a59d839c87fa8ded91a6adc04bfd42b5f0c85038
Jahid Anowar <milon5143@gmail.com>
2021-02-06 14:34:55 +0530

    Navigation Created

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,26 +1,15 @@
 <template>
-  <router-view/>
+  <div>
+    <app-bar></app-bar>
+    <router-view/>
+  </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+import AppBar from '@/components/AppBar.vue'
+export default {
+  components:{
+    AppBar
+  }
 }
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+</script>
---
+++ frontend/src/components/AppBar.vue
@@ -0,0 +1,32 @@
+<template> 
+    <header class="bg-white">
+        <div class="container mx-auto py-5 sm:px-0 flex flex-wrap justify-center sm:justify-between">
+            <router-link class="block content-center self-center font-bold text-2xl" to="/">LaraVueSaaS</router-link>
+            <nav>
+                <ul class="inline-flex justify-between">
+                    <li class="p-5">
+                        <router-link to="/">Pricing</router-link>
+                    </li>
+                    <li class="p-5">
+                        <router-link to="/">Signup</router-link>
+                    </li>
+                    <li class="p-5">
+                        <router-link to="/">Dashboard</router-link>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+
+    </header>
+</template>
+
+<script>
+export default {
+    name:"AppBar",
+
+}
+</script>
+
+<style>
+
+</style>
