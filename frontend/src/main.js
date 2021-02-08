cab25571a337dc5264e46c1ed20f97704dd08894
Jahid Anowar <milon5143@gmail.com>
2021-02-08 09:31:57 +0530

    Comment Added

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,10 +1,14 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import axios from 'axios'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import axios from "axios";
 
-axios.defaults.baseURL = 'http://localhost:8000';
+axios.defaults.baseURL = "http://localhost:8000";
 axios.defaults.withCredentials = true;
 
-createApp(App).use(store).use(router).mount('#app')
+// Checking if the Cookie is authenticated
+store.dispatch("auth/user").then(() => {
+    //Mounting the app
+    createApp(App).use(store).use(router).mount("#app");
+});
--- frontend/src/store/auth.js
+++ frontend/src/store/auth.js
@@ -7,23 +7,29 @@ export default {
     user: null,
   }),
   getters: {
+    // Return Authenticated state
     isAuthenticated: (state) => state.authenticated,
+    // Return Authenticated User State
     user: (state) => state.user
   },
   mutations: {
+    //Set authenticated state to true
     SET_AUTH(state, data) {
       state.authenticated = data;
     },
+    //Set user state with user data
     SET_USER(state, data) {
       state.user = data;
     },
   },
   actions: {
+    //Getting the CSRF Cookie and then logging in
     async login({ dispatch }, payload) {
       await axios.get("/sanctum/csrf-cookie");
       await axios.post("/api/login", payload);
       return dispatch("user");
     },
+    //Getting authenticated user from the API and setting it to the state
     user({ commit }) {
       axios
         .get("/api/user")
